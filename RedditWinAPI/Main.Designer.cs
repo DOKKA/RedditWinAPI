83d18cfb3e84e02a9fb1a9849e352633aae98232
Kevin Grieger <scuffmark@gmail.com>
2021-01-31 23:27:03 -0600

    it actually works!

--- RedditWinAPI/Main.Designer.cs
+++ RedditWinAPI/Main.Designer.cs
@@ -29,25 +29,21 @@
 		private void InitializeComponent()
 		{
 			this.sptOuter = new System.Windows.Forms.SplitContainer();
-			this.sptComments = new System.Windows.Forms.SplitContainer();
 			this.sptInner = new System.Windows.Forms.SplitContainer();
 			this.sptSub = new System.Windows.Forms.SplitContainer();
 			this.lbSubs = new System.Windows.Forms.ListBox();
 			this.lbPosts = new System.Windows.Forms.ListBox();
-			this.tvComments = new System.Windows.Forms.TreeView();
-			this.tbComment = new System.Windows.Forms.TextBox();
-			this.txtTitle = new System.Windows.Forms.TextBox();
-			this.txtBody = new System.Windows.Forms.TextBox();
-			this.btnOpen = new System.Windows.Forms.Button();
 			this.btnView = new System.Windows.Forms.Button();
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.txtBody = new System.Windows.Forms.TextBox();
+			this.txtTitle = new System.Windows.Forms.TextBox();
+			this.sptComments = new System.Windows.Forms.SplitContainer();
+			this.tvComments = new System.Windows.Forms.TreeView();
+			this.txtComment = new System.Windows.Forms.TextBox();
 			((System.ComponentModel.ISupportInitialize)(this.sptOuter)).BeginInit();
 			this.sptOuter.Panel1.SuspendLayout();
 			this.sptOuter.Panel2.SuspendLayout();
 			this.sptOuter.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.sptComments)).BeginInit();
-			this.sptComments.Panel1.SuspendLayout();
-			this.sptComments.Panel2.SuspendLayout();
-			this.sptComments.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.sptInner)).BeginInit();
 			this.sptInner.Panel1.SuspendLayout();
 			this.sptInner.Panel2.SuspendLayout();
@@ -56,6 +52,10 @@
 			this.sptSub.Panel1.SuspendLayout();
 			this.sptSub.Panel2.SuspendLayout();
 			this.sptSub.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.sptComments)).BeginInit();
+			this.sptComments.Panel1.SuspendLayout();
+			this.sptComments.Panel2.SuspendLayout();
+			this.sptComments.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// sptOuter
@@ -75,24 +75,6 @@
 			this.sptOuter.SplitterDistance = 500;
 			this.sptOuter.TabIndex = 0;
 			// 
-			// sptComments
-			// 
-			this.sptComments.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.sptComments.Location = new System.Drawing.Point(0, 0);
-			this.sptComments.Name = "sptComments";
-			this.sptComments.Orientation = System.Windows.Forms.Orientation.Horizontal;
-			// 
-			// sptComments.Panel1
-			// 
-			this.sptComments.Panel1.Controls.Add(this.tvComments);
-			// 
-			// sptComments.Panel2
-			// 
-			this.sptComments.Panel2.Controls.Add(this.tbComment);
-			this.sptComments.Size = new System.Drawing.Size(280, 561);
-			this.sptComments.SplitterDistance = 300;
-			this.sptComments.TabIndex = 0;
-			// 
 			// sptInner
 			// 
 			this.sptInner.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -111,7 +93,7 @@
 			this.sptInner.Panel2.Controls.Add(this.txtBody);
 			this.sptInner.Panel2.Controls.Add(this.txtTitle);
 			this.sptInner.Size = new System.Drawing.Size(500, 561);
-			this.sptInner.SplitterDistance = 250;
+			this.sptInner.SplitterDistance = 249;
 			this.sptInner.TabIndex = 0;
 			// 
 			// sptSub
@@ -127,7 +109,7 @@
 			// sptSub.Panel2
 			// 
 			this.sptSub.Panel2.Controls.Add(this.lbPosts);
-			this.sptSub.Size = new System.Drawing.Size(500, 250);
+			this.sptSub.Size = new System.Drawing.Size(500, 249);
 			this.sptSub.SplitterDistance = 150;
 			this.sptSub.TabIndex = 0;
 			// 
@@ -137,7 +119,7 @@
 			this.lbSubs.FormattingEnabled = true;
 			this.lbSubs.Location = new System.Drawing.Point(0, 0);
 			this.lbSubs.Name = "lbSubs";
-			this.lbSubs.Size = new System.Drawing.Size(150, 250);
+			this.lbSubs.Size = new System.Drawing.Size(150, 249);
 			this.lbSubs.TabIndex = 0;
 			// 
 			// lbPosts
@@ -146,25 +128,40 @@
 			this.lbPosts.FormattingEnabled = true;
 			this.lbPosts.Location = new System.Drawing.Point(0, 0);
 			this.lbPosts.Name = "lbPosts";
-			this.lbPosts.Size = new System.Drawing.Size(346, 250);
+			this.lbPosts.Size = new System.Drawing.Size(346, 249);
 			this.lbPosts.TabIndex = 0;
 			// 
-			// tvComments
+			// btnView
 			// 
-			this.tvComments.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tvComments.Location = new System.Drawing.Point(0, 0);
-			this.tvComments.Name = "tvComments";
-			this.tvComments.Size = new System.Drawing.Size(280, 300);
-			this.tvComments.TabIndex = 0;
+			this.btnView.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnView.Location = new System.Drawing.Point(422, 281);
+			this.btnView.Name = "btnView";
+			this.btnView.Size = new System.Drawing.Size(75, 23);
+			this.btnView.TabIndex = 3;
+			this.btnView.Text = "View Link";
+			this.btnView.UseVisualStyleBackColor = true;
 			// 
-			// tbComment
+			// btnOpen
 			// 
-			this.tbComment.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tbComment.Location = new System.Drawing.Point(0, 0);
-			this.tbComment.Multiline = true;
-			this.tbComment.Name = "tbComment";
-			this.tbComment.Size = new System.Drawing.Size(280, 257);
-			this.tbComment.TabIndex = 0;
+			this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnOpen.Location = new System.Drawing.Point(3, 281);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(104, 23);
+			this.btnOpen.TabIndex = 2;
+			this.btnOpen.Text = "Open in browser";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			// 
+			// txtBody
+			// 
+			this.txtBody.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBody.Location = new System.Drawing.Point(3, 26);
+			this.txtBody.Multiline = true;
+			this.txtBody.Name = "txtBody";
+			this.txtBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtBody.Size = new System.Drawing.Size(494, 249);
+			this.txtBody.TabIndex = 1;
 			// 
 			// txtTitle
 			// 
@@ -175,36 +172,41 @@
 			this.txtTitle.Size = new System.Drawing.Size(494, 20);
 			this.txtTitle.TabIndex = 0;
 			// 
-			// txtBody
+			// sptComments
 			// 
-			this.txtBody.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.txtBody.Location = new System.Drawing.Point(3, 26);
-			this.txtBody.Multiline = true;
-			this.txtBody.Name = "txtBody";
-			this.txtBody.Size = new System.Drawing.Size(494, 248);
-			this.txtBody.TabIndex = 1;
+			this.sptComments.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.sptComments.Location = new System.Drawing.Point(0, 0);
+			this.sptComments.Name = "sptComments";
+			this.sptComments.Orientation = System.Windows.Forms.Orientation.Horizontal;
 			// 
-			// btnOpen
+			// sptComments.Panel1
 			// 
-			this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.btnOpen.Location = new System.Drawing.Point(3, 280);
-			this.btnOpen.Name = "btnOpen";
-			this.btnOpen.Size = new System.Drawing.Size(104, 23);
-			this.btnOpen.TabIndex = 2;
-			this.btnOpen.Text = "Open in browser";
-			this.btnOpen.UseVisualStyleBackColor = true;
+			this.sptComments.Panel1.Controls.Add(this.tvComments);
 			// 
-			// btnView
+			// sptComments.Panel2
 			// 
-			this.btnView.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnView.Location = new System.Drawing.Point(422, 280);
-			this.btnView.Name = "btnView";
-			this.btnView.Size = new System.Drawing.Size(75, 23);
-			this.btnView.TabIndex = 3;
-			this.btnView.Text = "View Link";
-			this.btnView.UseVisualStyleBackColor = true;
+			this.sptComments.Panel2.Controls.Add(this.txtComment);
+			this.sptComments.Size = new System.Drawing.Size(280, 561);
+			this.sptComments.SplitterDistance = 300;
+			this.sptComments.TabIndex = 0;
+			// 
+			// tvComments
+			// 
+			this.tvComments.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tvComments.Location = new System.Drawing.Point(0, 0);
+			this.tvComments.Name = "tvComments";
+			this.tvComments.Size = new System.Drawing.Size(280, 300);
+			this.tvComments.TabIndex = 0;
+			// 
+			// txtComment
+			// 
+			this.txtComment.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtComment.Location = new System.Drawing.Point(0, 0);
+			this.txtComment.Multiline = true;
+			this.txtComment.Name = "txtComment";
+			this.txtComment.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtComment.Size = new System.Drawing.Size(280, 257);
+			this.txtComment.TabIndex = 0;
 			// 
 			// Main
 			// 
@@ -218,11 +220,6 @@
 			this.sptOuter.Panel2.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.sptOuter)).EndInit();
 			this.sptOuter.ResumeLayout(false);
-			this.sptComments.Panel1.ResumeLayout(false);
-			this.sptComments.Panel2.ResumeLayout(false);
-			this.sptComments.Panel2.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.sptComments)).EndInit();
-			this.sptComments.ResumeLayout(false);
 			this.sptInner.Panel1.ResumeLayout(false);
 			this.sptInner.Panel2.ResumeLayout(false);
 			this.sptInner.Panel2.PerformLayout();
@@ -232,6 +229,11 @@
 			this.sptSub.Panel2.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.sptSub)).EndInit();
 			this.sptSub.ResumeLayout(false);
+			this.sptComments.Panel1.ResumeLayout(false);
+			this.sptComments.Panel2.ResumeLayout(false);
+			this.sptComments.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.sptComments)).EndInit();
+			this.sptComments.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -249,6 +251,6 @@
 		private System.Windows.Forms.TextBox txtTitle;
 		private System.Windows.Forms.SplitContainer sptComments;
 		private System.Windows.Forms.TreeView tvComments;
-		private System.Windows.Forms.TextBox tbComment;
+		private System.Windows.Forms.TextBox txtComment;
 	}
 }
--- RedditWinAPI/Main.cs
+++ RedditWinAPI/Main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reddit.Controllers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,72 @@ namespace RedditWinAPI
 {
 	public partial class Main : Form
 	{
+		RedditService rs;
 		public Main()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			ConfigRoot config = ConfigService.GetConfig();
+
+			tvComments.BeforeExpand += TvComments_BeforeExpand;
+			tvComments.AfterSelect += TvComments_AfterSelect;
+			lbSubs.SelectedIndexChanged += lbSubs_SelectedIndexChanged;
+			lbPosts.SelectedIndexChanged += lbPosts_SelectedIndexChanged;
+
+
+			rs = new RedditService(config.settings);
+			SubRedditItem[] subs = rs.GetSubReddits(config.preferences.subreddits.ToArray());
+			lbSubs.Items.AddRange(subs);
+		}
+
+		private void TvComments_AfterSelect(object sender, TreeViewEventArgs e)
+		{
+			txtComment.Text = e.Node.Text;
+		}
+
+		private void TvComments_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+		{
+			e.Node.Nodes.Clear();
+			CommentNode[] nodes = rs.GetCommentNodes((CommentNode)e.Node);
+			e.Node.Nodes.AddRange(nodes);
+		}
+
+		private void lbSubs_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			lbPosts.Items.Clear();
+			tvComments.Nodes.Clear();
+			txtBody.Text = "";
+			txtTitle.Text = "";
+			if (lbSubs.SelectedItems.Count > 0)
+			{
+				SubRedditItem item = (SubRedditItem)lbSubs.SelectedItems[0];
+				PostItem[] items = rs.GetTopPosts(item);
+				lbPosts.Items.AddRange(items);
+			}
+		}
+
+		private void lbPosts_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			tvComments.Nodes.Clear();
+			if (lbPosts.SelectedItems.Count > 0)
+			{
+				PostItem item = (PostItem)lbPosts.SelectedItems[0];
+				txtTitle.Text = item.Post.Title;
+
+				if (item.Post.GetType() == typeof(SelfPost))
+				{
+					txtBody.Text = ((SelfPost)item.Post).SelfText.Replace("\n", Environment.NewLine);
+				}
+				else
+				{
+					txtBody.Text = item.Post.GetType().FullName;
+				}
+
+				tvComments.Nodes.AddRange(rs.GetCommentNodes(item));
+			}
 		}
 	}
 }
